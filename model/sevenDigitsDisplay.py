96dd1fe47d37873000b3d9e3d03f4c00f534f891
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 21:28:44 +0200

    code ready. Documentation and ready to go

--- model/sevenDigitsDisplay.py
+++ model/sevenDigitsDisplay.py
@@ -1,7 +1,25 @@
 class SevenDigitsDisplay:
 
-	itoSD2 = [
-		[True, True, True, True, True, True, False] # 0
+	OFF = 16
+
+	itoDisplay = [
+		[True, True, True, True, True, True, False], # 0
+		[False, True, True, False, False, False, False], # 1
+		[True, True, False, True, True, False, True], # 2
+		[True, True, True, True, False, False, True], # 3
+		[False, True, True, False, False, True, True], # 4
+		[True, False, True, True, False, True, True], # 5
+		[True, False, True, True, True, True, True], # 6
+		[True, True, True, False, False, False, False], # 7
+		[True, True, True, True, True, True, True], # 8
+		[True, True, True, True, False, True, True], # 9
+		[True, True, True, False, True, True, True], # A
+		[False, False, True, True, True, True, True], # B
+		[True, False, False, True, True, True, False], # C
+		[False, True, True, True, True, False, True], # D
+		[True, False, False, True, True, True, True], # E
+		[True, False, False, False, True, True, True], # F
+		[False, False, False, False, False, False, False] # EMPTY
 	]
 
 	def __init__(self, v=None):
@@ -15,31 +33,53 @@ class SevenDigitsDisplay:
 			"G": False
 		}
 
-		if v == None:
-			return
+		if v == None or v >= len(self.itoDisplay):
+			v = self.OFF
+		
+		self.v = v
 		for i in range(7):
-			# print(chr(i + ord("A")) + str(self.itoSD2[v][i]))
-			self.digits[chr(i + ord("A"))] = self.itoSD2[v][i]
+			self.digits[chr(i + ord("A"))] = self.itoDisplay[v][i]
+
+	def getStates(self):
+		return self.itoDisplay[self.v]
 
 	def __str__(self):
-		return f" {self.get('A')} \n{self.get('F')}  {self.get('B')}\n {self.get('G')} \n{self.get('E')}  {self.get('C')}\n {self.get('D')} "
+		return f" {self.getS('A')} \n{self.getS('F')}  {self.getS('B')}\n {self.getS('G')} \n{self.getS('E')}  {self.getS('C')}\n {self.getS('D')} "
 
 	def get(self, key):
-		if key in ["A", "G", "D"]:
-			return SevenDigitsDisplay.hori(self.digits[key])
-		else:
-			return SevenDigitsDisplay.verti(self.digits[key])
+		return self.digits[key]
 
-	@staticmethod
-	def hori(val):
-		return "──" if val else "  "
-	
-	@staticmethod
-	def verti(val):
-		return "│" if val else " "
+	def getS(self, key):
+		if key in ["A", "G", "D"]:
+			return "──" if self.get(key) else "  "
+		else:
+			return "│" if self.get(key) else " "
 
 
 
 if __name__ == "__main__":
-	d = SevenDigitsDisplay(0)
-	print(d)
+	for i in range(0, 16 + 1):
+		d = SevenDigitsDisplay(i)
+		print("--------------")
+		print(d)
+		print("--------------")
+	
+
+	digito = SevenDigitsDisplay(9)
+	print("9")
+	print(digito)
+	print("9")
+	print(digito.getStates())
+
+	if digito.get("A") == True:
+		## Set LED ON
+		pass
+	else:
+		## Set LED OFF
+		pass
+
+	digito2 = SevenDigitsDisplay(SevenDigitsDisplay.OFF)
+	print("off")
+	print(digito2)
+	print("off")
+	print(digito2.getStates())
