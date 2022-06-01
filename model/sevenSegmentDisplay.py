1524fb2452ab50b15492bd19968f85d4aefb9f53
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-01 22:55:15 +0200

    sevenSegmentDisplay commented

--- model/sevenSegmentDisplay.py
+++ model/sevenSegmentDisplay.py
@@ -1,4 +1,5 @@
-class SevenDigitsDisplay:
+class SevenSegmentDisplay:
+	'''Class with the logic need to parse a number to a seven segments display'''
 
 	OFF = 16
 
@@ -22,38 +23,35 @@ class SevenDigitsDisplay:
 		[False, False, False, False, False, False, False] # EMPTY
 	]
 
-	def __init__(self, v=None):
-		self.digits = {
-			"A": False,
-			"B": False,
-			"C": False,
-			"D": False,
-			"E": False,
-			"F": False,
-			"G": False
-		}
+	def __init__(self, v:int=None) -> None:
+		self.segments = {}
 		self.update(v)
 
-	def update(self, v):
-		if not (type(v) is int) or v < 0 or v > SevenDigitsDisplay.OFF:
+	def update(self, v:int) -> None:
+		'''Update the display with the given value. If invalid, the display is cleared'''
+		if not (type(v) is int) or v < 0 or v > SevenSegmentDisplay.OFF:
 			v = self.OFF
 		self.v = v
 		for i in range(7):
-			self.digits[chr(i + ord("A"))] = self.itoDisplay[v][i]
+			self.segments[chr(i + ord("A"))] = self.itoDisplay[v][i]
 
-	def clear(self):
-		self.update(SevenDigitsDisplay.OFF)
+	def clear(self) -> None:
+		'''Clears the display'''
+		self.update(SevenSegmentDisplay.OFF)
 
 	def getStates(self):
+		'''Returns a list with the states of the seven segments display'''
 		return self.itoDisplay[self.v]
 
 	def __str__(self):
 		return f" {self.getS('A')} \n{self.getS('F')}  {self.getS('B')}\n {self.getS('G')} \n{self.getS('E')}  {self.getS('C')}\n {self.getS('D')} "
 
-	def get(self, key):
-		return self.digits[key]
+	def get(self, key:str) -> bool:
+		'''Returns the state of the segment with the given key.'''
+		return self.segments[key]
 
-	def getS(self, key):
+	def getS(self, key:str) -> str:
+		'''Returns the state of the segment with the given key as a string.'''
 		if key in ["A", "G", "D"]:
 			return "──" if self.get(key) else "  "
 		else:
@@ -63,13 +61,13 @@ class SevenDigitsDisplay:
 
 if __name__ == "__main__":
 	for i in range(0, 16 + 1):
-		d = SevenDigitsDisplay(i)
+		d = SevenSegmentDisplay(i)
 		print("--------------")
 		print(d)
 		print("--------------")
 	
 
-	digito = SevenDigitsDisplay(9)
+	digito = SevenSegmentDisplay("fsaf")
 	print("9")
 	print(digito)
 	print("9")
@@ -90,7 +88,7 @@ if __name__ == "__main__":
 		## Set LED OFF
 		pass
 
-	digito2 = SevenDigitsDisplay(SevenDigitsDisplay.OFF)
+	digito2 = SevenSegmentDisplay(SevenSegmentDisplay.OFF)
 	print("off")
 	print(digito2)
 	print("off")
