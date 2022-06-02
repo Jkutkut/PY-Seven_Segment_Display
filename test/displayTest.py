83d7973a70c6cad66a7938b634a7dfcdec3fb86d
Jkutkut <jorgeregonzalez@gmail.com>
2022-06-02 16:28:36 +0200

    Small test for display added

--- test/displayTest.py
+++ test/displayTest.py
@@ -6,9 +6,15 @@
 #    By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/02 16:05:06 by jre-gonz          #+#    #+#              #
-#    Updated: 2022/06/02 16:05:07 by jre-gonz         ###   ########.fr        #
+#    Updated: 2022/06/02 16:24:44 by jre-gonz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+import sys
+sys.path.append('.')
+
+from model.sevenSegmentDisplay import SevenSegmentDisplay as SegmentDisplay
+from model.display import Display
 
 if __name__ == "__main__":
 	d = Display()
